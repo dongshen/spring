6bd236c0e4c0394d35d07a6bd008b09458497a02
dongshen <shendong70@163.com>
2014-12-07 22:47:51 +0800

    move error page to jsps/error

--- spring/exception-demo/src/main/java/com/sdong/spring/exception/demo/exception/MyExceptionHandler.java
+++ spring/exception-demo/src/main/java/com/sdong/spring/exception/demo/exception/MyExceptionHandler.java
@@ -18,11 +18,11 @@ public class MyExceptionHandler implements HandlerExceptionResolver {
 		
 		// 根据不同错误转向不同页面
 		if(ex instanceof BusinessException) {
-			return new ModelAndView("error-business", model);
+			return new ModelAndView("error/error-business", model);
 		}else if(ex instanceof ParameterException) {
-			return new ModelAndView("error-parameter", model);
+			return new ModelAndView("error/error-parameter", model);
 		} else {
-			return new ModelAndView("error", model);
+			return new ModelAndView("error/error", model);
 		}
 	}
 }
